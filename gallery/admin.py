7f49c16bc471738afef9399cd5de7931d67a0d3f
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-02-02 21:54:51 +0300

    created view function which returns response

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,10 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# # -*- coding: utf-8 -*-
+# from __future__ import unicode_literals
 
-from django.contrib import admin
+# from django.contrib import admin
+# from .models import Image,Category,Location
 
-# Register your models here.
+# # Register your models here.
+# admin.site.register(Image)
+# admin.site.register(Category)
+# admin.site.register(Location)
--- gallery/views.py
+++ gallery/views.py
@@ -2,5 +2,6 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
