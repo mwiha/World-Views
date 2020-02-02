8dae87baf2e737c77b7d103491c7989e0af7a707
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-02-02 22:03:12 +0300

    created url functions in gallery app

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,10 +1,8 @@
-# # -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
+from django.contrib import admin
+from .models import Image,Category,Location
 
-# from django.contrib import admin
-# from .models import Image,Category,Location
+# Register your models here.
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
 
-# # Register your models here.
-# admin.site.register(Image)
-# admin.site.register(Category)
-# admin.site.register(Location)
