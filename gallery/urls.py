b7aa14125974a26d20008f26663811afa9d96021
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-02-03 12:53:39 +0300

    added the urlconf for app gallery

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url(r'^$', views.index, name='index'),
+    url(r'^gallery$', views.gallery, name='gallery'),
+    url(r'^search/', views.search_category, name = 'search_category')
 ]
